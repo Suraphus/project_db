d0683988bf182a97e007a44140fea7ac47ec0cd2
Suraphus <Suraphus9989@gmail.com>
2026-02-25 11:19:46 +0700

    Change FacilityCard

--- frontend/src/pages/FacilitiesPage.jsx
+++ frontend/src/pages/FacilitiesPage.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import FacilityCard from "../components/FacilityCard";
 
 function Facilities() {
+  const navigate = useNavigate();
   const data = [
     {
       img: "https://cdn.prod.website-files.com/65724dca4c6e2916c2b00c25/65e9a454bcdf8bb397b81543_Howe_Final_4.jpg",
@@ -29,13 +31,46 @@ function Facilities() {
   ];
 
   return (
-    <div className="px-[60px] py-[40px]">
-      <p className="text-[#005E40] text-4xl font-bold ...">Facilities</p>
+    <div className="min-h-screen bg-[#f4f6f9] py-16 px-6">
+      <div className="max-w-6xl mx-auto">
+        <div className="text-center mb-14">
+          <h1 className="text-5xl font-bold text-gray-800">
+            Choose your sports
+          </h1>
+          {/* <div className="w-24 h-1 bg-[#005E40] mx-auto mt-4 rounded-full"></div>
+          {/* <p className="mt-6 text-gray-500 text-lg max-w-2xl mx-auto">
+            Select your preferred sport and reserve your field quickly and
+            conveniently.
+          </p> */}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-3xl shadow-md hover:shadow-2xl transition duration-300 overflow-hidden group"
+            >
+              <div className="h-56 overflow-hidden">
+                <img
+                  src={item.img}
+                  alt={item.name}
+                  className="w-full h-full object-cover group-hover:scale-110 transition duration-500"
+                />
+              </div>
 
-      <div className="bg-[#e6e6e6] p-10 rounded-[5px] grid grid-cols-3 gap-10 shadow-lg">
-        {data.map((item, index) => (
-          <FacilityCard key={index} img={item.img} name={item.name} />
-        ))}
+              <div className="p-6 text-center">
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {item.name}
+                </h2>
+                <button
+                  onClick={() => navigate("/fields")}
+                  className="mt-4 px-6 py-2 bg-[#005E40] text-white rounded-full hover:bg-[#014d34] transition hover:cursor-pointer hover:scale-105gi"
+                >
+                  Book Now
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
